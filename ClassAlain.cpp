743e0467e08949e443a8a12a673088c4133444d1
alismont <alain.lismont@gmail.com>
2016-09-18 23:03:59 +0200

    Add files via upload

--- ClassAlain.cpp
+++ ClassAlain.cpp
@@ -11,16 +11,24 @@
 class ClassAlain {
 
 public:   //public 
+    //-- scan
     int i;
     unsigned long T1SetUp=0;
     unsigned long cycleTime = 0;
     unsigned long CyclePrescedent = 0;
     unsigned long Cycle = 0;
     
+    //--tempo
+    int Presc;
+    int Accu;
+    boolean DN_Bit;
+    boolean TT_Bit;
+    boolean Condition;
+    
  ClassAlain() {  //the constructor
  
  }
- 
+ //----- fonction calcul du scanning-------------
     unsigned long TpsScan(unsigned long _T1SetUp){
 
   cycleTime = millis() - _T1SetUp;
@@ -28,6 +36,36 @@ public:   //public
   CyclePrescedent = cycleTime;
   
     return Cycle;
+}
+ //----- fonction Tempo TON-------------
+  
+ boolean Ton(int _Presc,boolean _Condition,int _Accu){
+    boolean _DN_Bit;
+    boolean _TT_Bit;
+    boolean _CptT;
+    
+    //_Presc=Presc;
+    //_Accu=Accu;
+    //_Condition=Condition;
+    //_DN_Bit=DN_Bit;
+    //_TT_Bit=TT_Bit;
+    
+//--code--    
+if (_Condition) {
+    _CptT = true;
+  }
+  else {
+    _Accu =  0.0;
+    _DN_Bit =  0.0;
+    _CptT = false;
+  }
+  if (_Accu >= _Presc) { // présélection de 600 donc 60 secondes
+    _DN_Bit =  1.0;   // bit down
+    _Accu = _Presc;
+  }
+
+  return _DN_Bit; 
+
 }
 
 };
--- Class_Alain.ino
+++ Class_Alain.ino
@@ -1,4 +1,5 @@
 #include "ClassAlain.cpp"
+#include "TimerOne.h"
 
 //-------------------------------------------------------------------
 // attention si class sans paramètres pas myClassAlain().....
@@ -7,16 +8,87 @@ ClassAlain myClassAlain;
 unsigned long T1=0;
 unsigned long Scan=0;
 
+boolean TDN[10];
+int Presc[10];
+int Accu[10];
+boolean Cond[10];
+
+int LED13=13;
+int var=0;
+int PIN38=38;
+
+//---------------------------------
 void setup(){
 
+pinMode(LED13, OUTPUT); 
+pinMode(PIN38, INPUT);
+
 Serial.begin(9600);
+
+    Timer1.initialize(100000);         // initialize timer1
+  //Timer1.setPeriod(100);
+  Timer1.attachInterrupt(callback);
+
+  //--prescelection tempo---10=1sec
+  Presc[1]=50;Accu[1]=0;
+  var=1;
+  Presc[2]=10;Accu[2]=0;
 T1=millis();
 
 }
-
+//---------------------------------
 void loop(){
-delay(1000);
-Scan=myClassAlain.TpsScan(T1);
-Serial.println(Scan);
 
+switch (var) {
+    case 1:
+      Cond[1]=true;
+        if(TDN[1])
+        {
+        var=2;
+        Cond[1]=false;Accu[1]=0;
+        digitalWrite(LED13,HIGH);
+        }
+      break;
+    case 2:
+      Cond[1]=true;
+        if(TDN[1])
+        {
+        var=1;
+        Cond[1]=false;Accu[1]=0;
+        digitalWrite(LED13,LOW);
+        }      
+      break;
+  }
+  
+ //----- input 38 tempo led13----- 
+  if(digitalRead(PIN38))
+  {
+   Cond[2]=true;
+
+        if(TDN[2])
+        {
+                digitalWrite(LED13,LOW);
+        }
+  } 
+  else{
+    Cond[2]=false;Accu[2]=0; 
+  }
+  
+ //---gestion tempo---- 
+TDN[1]=myClassAlain.Ton(Presc[1],Cond[1],Accu[1]);
+TDN[2]=myClassAlain.Ton(Presc[2],Cond[2],Accu[2]);
+
+Scan=myClassAlain.TpsScan(T1);
+Serial.println(var);
+
+}
+//---------------------------------
+//--------- interrupts-------------
+void callback() {
+    if(Cond[1]){
+        Accu[1]=Accu[1]+1;
+    }
+    if(Cond[2]){
+        Accu[2]=Accu[2]+1;
+    }
 }
